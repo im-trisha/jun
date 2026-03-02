9753f0f7cf9cad8742cd659e5bce0c840a152779
imtrisha <imtrisha@noreply.codeberg.org>
2026-03-02 11:11:41 +0100

    initial commit

---
+++ mdrg/src/lib.rs
@@ -0,0 +1,20 @@
+//! Serde deserialization models for MDRG game save files.
+//! Currently, only supports 0.95, in the future may support
+//! older and newer versions
+//!
+//!
+//! Types are extracted from the IL2CPP disassembly
+//! each module groups related C# classes
+//!
+//! As a sidenote, I don't know a lot about unity games RE,
+//! neither I know if the TypeDefIndex is constant between
+//! IL2CPP disassembly runs, but I kept it for at least helping myself
+//! going back and forth through the dump code
+//!
+//! # Usage
+//!
+//! ```rust,no_run
+//! let json = std::fs::read_to_string("save.mdrg").unwrap();
+//! let save: mdrg::GameVariables = serde_json::from_str(&json).unwrap();
+//! println!("{}", save.player_name);
+//! ```
