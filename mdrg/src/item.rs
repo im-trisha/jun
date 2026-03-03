c1597874273f345f85201565e69c8c996a134bfa
imtrisha <imtrisha@noreply.codeberg.org>
2026-03-03 16:29:42 +0100

    fix: hopefully now item.rs is actually not there

--- mdrg/src/item.rs
+++
@@ -1,8 +0,0 @@
-//! Item types
-
-use serde::{Deserialize, Serialize};
-
-#[cfg_attr(feature = "derive-clone", derive(Clone))]
-#[cfg_attr(feature = "derive-debug", derive(Debug))]
-#[derive(Serialize, Deserialize, Default)]
-pub struct ItemsState {}
