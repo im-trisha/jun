51b5bd338f86f6193223a7bcf6b9e737169c0dc9
imtrisha <imtrisha@noreply.codeberg.org>
2026-03-17 15:13:27 +0100

    feat: move to i18n

--- jun/src/views/jun_stats.rs
+++ jun/src/views/jun_stats.rs
@@ -7,37 +7,38 @@ pub struct JunStats {}
 
 impl JunStats {
     pub fn ui(&mut self, ui: &mut egui::Ui, state: &mut JunAppState) {
+        let lang = state.language;
         let Some(slot) = state.working_save_slot() else {
             return;
         };
-        let data = try_i18n!(state, slot.save_data(), state.t_error_to_json());
+        let data = try_i18n!(state, slot.save_data(), lang.t_error_to_json());
 
         text_column!(
             ui,
-            "Name",
-            "Jun is a lovely girlfriend, you'd be a real gentleman keeping her name as such :)",
+            lang.t_mdrgp_bot_name_title(),
+            lang.t_mdrgp_bot_name_desc(),
             &mut data.bot_name
         );
 
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            stat_column!(cols[0], "Lust", &mut data.lust, 0..=i32::MAX);
-            stat_column!(cols[1], "Current horniness", "Jun's current arousal level", &mut data.current_horniness, 0.0..=1.0);
-            stat_column!(cols[2], "Longing", "Jun's accumulated horniness", &mut data.longing, 0..=10);
+            stat_column!(cols[0], lang.t_mdrgp_lust(), &mut data.lust, 0..=i32::MAX);
+            stat_column!(cols[1], lang.t_mdrgp_current_horniness_title(), lang.t_mdrgp_current_horniness_desc(), &mut data.current_horniness, 0.0..=1.0);
+            stat_column!(cols[2], lang.t_mdrgp_longing_title(), lang.t_mdrgp_longing_desc(), &mut data.longing, 0..=10);
         });
 
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            stat_column!(cols[0], "Sympathy", "How much she likes you", &mut data.sympathy, 0..=i32::MAX);
-            stat_column!(cols[1], "Mood", "Jun's current mood", &mut data.mood, -1.0..=1.0);
-            stat_column!(cols[2], "Intelligence", &mut data.intelligence, 0..=i32::MAX);
+            stat_column!(cols[0], lang.t_mdrgp_sympathy_title(), lang.t_mdrgp_sympathy_desc(), &mut data.sympathy, 0..=i32::MAX);
+            stat_column!(cols[1], lang.t_mdrgp_mood_title(), lang.t_mdrgp_mood_desc(), &mut data.mood, -1.0..=1.0);
+            stat_column!(cols[2], lang.t_mdrgp_intelligence(), &mut data.intelligence, 0..=i32::MAX);
         });
 
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            stat_column!(cols[0], "Cum inside", "Volume of sperm inside Jun's vagina (mL)", &mut data.cum_inside, 0.0..=f32::MAX);
-            stat_column!(cols[1], "Cum inside anal", "Volume of sperm inside Jun's anus (mL)", &mut data.cum_inside_anal, 0.0..=f32::MAX);
-            stat_column!(cols[2], "Cum inside stomach", "Volume of sperm inside Jun's stomach (mL)", &mut data.cum_inside_stomach, 0.0..=f32::MAX);
+            stat_column!(cols[0], lang.t_mdrgp_cum_inside_title(), lang.t_mdrgp_cum_inside_desc(), &mut data.cum_inside, 0.0..=f32::MAX);
+            stat_column!(cols[1], lang.t_mdrgp_cum_inside_anal_title(), lang.t_mdrgp_cum_inside_anal_desc(), &mut data.cum_inside_anal, 0.0..=f32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_cum_inside_stomach_title(), lang.t_mdrgp_cum_inside_stomach_desc(), &mut data.cum_inside_stomach, 0.0..=f32::MAX);
         });
     }
 }
--- jun/src/views/player_stats.rs
+++ jun/src/views/player_stats.rs
@@ -7,134 +7,135 @@ pub struct PlayerStats {}
 
 impl PlayerStats {
     pub fn ui(&mut self, ui: &mut egui::Ui, state: &mut JunAppState) {
+        let lang = state.language;
         let Some(slot) = state.working_save_slot() else {
             return;
         };
-        let data = try_i18n!(state, slot.save_data(), state.t_error_to_json());
+        let data = try_i18n!(state, slot.save_data(), lang.t_error_to_json());
 
         text_column!(
             ui,
-            "Name",
-            "The player's real name is anon",
+            lang.t_mdrgp_player_name_title(),
+            lang.t_mdrgp_player_name_desc(),
             &mut data.player_name
         );
 
         text_column!(
             ui,
-            "Status text",
-            "The status text shown on the top left, most of the time used for the rent, you can keep this empty",
+            lang.t_mdrgp_status_text_title(),
+            lang.t_mdrgp_status_text_desc(),
             &mut data.status_text
         );
 
-        heading_column!(ui, "Health");
+        heading_column!(ui, lang.t_mdrgp_heading_health());
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            stat_column!(cols[0], "Stamina", &mut data.stamina, 0.0..=1.0);
-            stat_column!(cols[1], "Mental health", &mut data.mental_health, 0.0..=1.0);
-            stat_column!(cols[2], "Mental health (temporary)", &mut data.mental_health_temporary, 0.0..=1.0);
+            stat_column!(cols[0], lang.t_mdrgp_stamina(), &mut data.stamina, 0.0..=1.0);
+            stat_column!(cols[1], lang.t_mdrgp_mental_health(), &mut data.mental_health, 0.0..=1.0);
+            stat_column!(cols[2], lang.t_mdrgp_mental_health_temporary(), &mut data.mental_health_temporary, 0.0..=1.0);
         });
 
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            stat_column!(cols[0], "Satiation", &mut data.satiation, 0.0..=1.0);
-            stat_column!(cols[1], "Health", &mut data.health, 0.0..=1.0);
-            stat_column!(cols[2], "Vinegara effect end", "The time (In game minutes) when Vinegara (drug) effect will end", &mut data.vinegara_effect_end, 0..=10);
+            stat_column!(cols[0], lang.t_mdrgp_satiation(), &mut data.satiation, 0.0..=1.0);
+            stat_column!(cols[1], lang.t_mdrgp_health(), &mut data.health, 0.0..=1.0);
+            stat_column!(cols[2], lang.t_mdrgp_vinegara_effect_end_title(), lang.t_mdrgp_vinegara_effect_end_desc(), &mut data.vinegara_effect_end, 0..=10);
         });
 
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            stat_column!(cols[0], "Death grip effect end", "The time (In game minutes) when Death Grip (drug) effect will end", &mut data.death_grip_effect_end, 0..=i32::MAX);
-            stat_column!(cols[1], "Max cum", "Desc", &mut data.max_cum, 0..=i32::MAX);
-            stat_column!(cols[2], "Remaining cum", "Desc", &mut data.remaining_cum, 0..=i32::MAX);
+            stat_column!(cols[0], lang.t_mdrgp_death_grip_effect_end_title(), lang.t_mdrgp_death_grip_effect_end_desc(), &mut data.death_grip_effect_end, 0..=i32::MAX);
+            stat_column!(cols[1], lang.t_mdrgp_max_cum_title(), lang.t_mdrgp_max_cum_desc(), &mut data.max_cum, 0..=i32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_remaining_cum_title(), lang.t_mdrgp_remaining_cum_desc(), &mut data.remaining_cum, 0..=i32::MAX);
         });
 
-        heading_column!(ui, "Minigames");
+        heading_column!(ui, lang.t_mdrgp_heading_minigames());
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            stat_column!(cols[0], "Old maid wins", &mut data.times_won_old_maid, 0..=i32::MAX);
-            stat_column!(cols[1], "Old maid losses", &mut data.times_lost_old_maid, 0..=i32::MAX);
-            stat_column!(cols[2], "Old maid ran away", &mut data.times_ran_away_old_maid, 0..=i32::MAX);
+            stat_column!(cols[0], lang.t_mdrgp_times_won_old_maid(), &mut data.times_won_old_maid, 0..=i32::MAX);
+            stat_column!(cols[1], lang.t_mdrgp_times_lost_old_maid(), &mut data.times_lost_old_maid, 0..=i32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_times_ran_away_old_maid(), &mut data.times_ran_away_old_maid, 0..=i32::MAX);
         });
 
         #[rustfmt::skip]
         ui.columns(4, |cols| {
-            stat_column!(cols[0], "Chess wins", &mut data.times_won_chess, 0..=i32::MAX);
-            stat_column!(cols[1], "Chess losses", &mut data.times_lost_chess, 0..=i32::MAX);
-            stat_column!(cols[2], "Word chain wins", &mut data.times_won_word_chain, 0..=i32::MAX);
-            stat_column!(cols[3], "Word chain losses", &mut data.times_lost_word_chain, 0..=i32::MAX);
+            stat_column!(cols[0], lang.t_mdrgp_times_won_chess(), &mut data.times_won_chess, 0..=i32::MAX);
+            stat_column!(cols[1], lang.t_mdrgp_times_lost_chess(), &mut data.times_lost_chess, 0..=i32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_times_won_word_chain(), &mut data.times_won_word_chain, 0..=i32::MAX);
+            stat_column!(cols[3], lang.t_mdrgp_times_lost_word_chain(), &mut data.times_lost_word_chain, 0..=i32::MAX);
         });
 
-        heading_column!(ui, "Nasty");
+        heading_column!(ui, lang.t_mdrgp_heading_nasty());
         #[rustfmt::skip]
         ui.columns(4, |cols| {
-            stat_column!(cols[0], "Times came inside (Vagina)", &mut data.times_came_inside, 0..=i32::MAX);
-            stat_column!(cols[1], "Times came inside (Anus)", &mut data.times_came_inside_anal, 0..=i32::MAX);
-            stat_column!(cols[2], "Times came inside (Mouth)", &mut data.times_came_in_mouth, 0..=i32::MAX);
-            stat_column!(cols[3], "Times came outside", &mut data.times_came_outside, 0..=i32::MAX);
+            stat_column!(cols[0], lang.t_mdrgp_times_came_inside(), &mut data.times_came_inside, 0..=i32::MAX);
+            stat_column!(cols[1], lang.t_mdrgp_times_came_inside_anal(), &mut data.times_came_inside_anal, 0..=i32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_times_came_in_mouth(), &mut data.times_came_in_mouth, 0..=i32::MAX);
+            stat_column!(cols[3], lang.t_mdrgp_times_came_outside(), &mut data.times_came_outside, 0..=i32::MAX);
         });
 
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            stat_column!(cols[0], "Volume (mL) mouth", &mut data.ml_came_in_mouth, 0.0..=f32::MAX);
-            stat_column!(cols[2], "Volume (mL) outside", "Volume (mL) ejaculated anywhere outside of Jun’s orifices", &mut data.ml_of_cum_wasted, 0.0..=f32::MAX);
+            stat_column!(cols[0], lang.t_mdrgp_ml_came_in_mouth(), &mut data.ml_came_in_mouth, 0.0..=f32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_ml_of_cum_wasted_title(), lang.t_mdrgp_ml_of_cum_wasted_desc(), &mut data.ml_of_cum_wasted, 0.0..=f32::MAX);
         });
 
-        heading_column!(ui, "Streaming/Economy");
+        heading_column!(ui, lang.t_mdrgp_heading_streaming_economy());
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            stat_column!(cols[0], "Stream count", "Total number of streams", &mut data.stream_count, 0..=i32::MAX);
-            stat_column!(cols[1], "Streamed for", "Total in game minutes spent streaming", &mut data.streamed_for, 0..=i32::MAX);
-            stat_column!(cols[2], "Donations money", "Total money earned from stream doations", &mut data.money_earned_from_donations, 0..=i32::MAX);
-        });
-
-        #[rustfmt::skip]
-        ui.columns(3, |cols| {
-            stat_column!(cols[0], "Longest stream", "Total in game minutes of the longest stream", &mut data.longest_stream, 0..=i32::MAX);
-            stat_column!(cols[1], "Subs", &mut data.subs, 0..=i32::MAX);
-            stat_column!(cols[2], "Followers", &mut data.followers, 0..=i32::MAX);
+            stat_column!(cols[0], lang.t_mdrgp_stream_count_title(), lang.t_mdrgp_stream_count_desc(), &mut data.stream_count, 0..=i32::MAX);
+            stat_column!(cols[1], lang.t_mdrgp_streamed_for_title(), lang.t_mdrgp_streamed_for_desc(), &mut data.streamed_for, 0..=i32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_money_earned_from_donations_title(), lang.t_mdrgp_money_earned_from_donations_desc(), &mut data.money_earned_from_donations, 0..=i32::MAX);
         });
 
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            stat_column!(cols[0], "Money", &mut data.money, 0..=i32::MAX);
-            stat_column!(cols[1], "Casino tokens", &mut data.casino_tokens, 0..=i32::MAX);
-            stat_column!(cols[2], "Last streamed at", "In-game minute of the last streaming session", &mut data.last_streamed_at, 0..=i32::MAX);
-        });
-
-        heading_column!(ui, "Time based stats");
-
-        #[rustfmt::skip]
-        ui.columns(3, |cols| {
-            stat_column!(cols[0], "Last work day", &mut data.last_worked_at_day, 0..=i32::MAX); // TODO: based on real day
-            stat_column!(cols[1], "Last church time", "The last time (In game minutes) when you went to church", &mut data.last_went_to_church_at, 0..=i32::MAX);
-            stat_column!(cols[2], "Last cuddle time", "The last time (In game minutes) when you cuddled with Jun", &mut data.last_cuddled_at, 0..=i32::MAX);
+            stat_column!(cols[0], lang.t_mdrgp_longest_stream_title(), lang.t_mdrgp_longest_stream_desc(), &mut data.longest_stream, 0..=i32::MAX);
+            stat_column!(cols[1], lang.t_mdrgp_subs(), &mut data.subs, 0..=i32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_followers(), &mut data.followers, 0..=i32::MAX);
         });
 
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            bool_column!(cols[0], "Slept with Jun", "Did you sleep with Jun yet?", &mut data.last_slept_with_bot);
-            stat_column!(cols[1], "Last woke up", "The last time (In game minutes) when you woke up", &mut data.last_woke_up_at, 0..=i32::MAX);
-            stat_column!(cols[2], "Last intercourse", "The last time (In game minutes) when you had an intercourse with Jun", &mut data.last_fucked_at, 0..=i32::MAX);
+            stat_column!(cols[0], lang.t_mdrgp_money(), &mut data.money, 0..=i32::MAX);
+            stat_column!(cols[1], lang.t_mdrgp_casino_tokens(), &mut data.casino_tokens, 0..=i32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_last_streamed_at_title(), lang.t_mdrgp_last_streamed_at_desc(), &mut data.last_streamed_at, 0..=i32::MAX);
+        });
+
+        heading_column!(ui, lang.t_mdrgp_heading_time_based_stats());
+
+        #[rustfmt::skip]
+        ui.columns(3, |cols| {
+            stat_column!(cols[0], lang.t_mdrgp_last_worked_at_day(), &mut data.last_worked_at_day, 0..=i32::MAX); // TODO: based on real day
+            stat_column!(cols[1], lang.t_mdrgp_last_went_to_church_at_title(), lang.t_mdrgp_last_went_to_church_at_desc(), &mut data.last_went_to_church_at, 0..=i32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_last_cuddled_at_title(), lang.t_mdrgp_last_cuddled_at_desc(), &mut data.last_cuddled_at, 0..=i32::MAX);
         });
 
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            stat_column!(cols[0], "Last interaction", "The last time (In game minutes) when you interacted with Jun", &mut data.last_interact_at, 0..=i32::MAX);
-            stat_column!(cols[1], "Last equipment session", "The last time (In game minutes) when you opened the equipment scene", &mut data.last_equipment_at, 0..=i32::MAX);
-            stat_column!(cols[2], "Last outside", "The last time (In game minutes) when you were outside with Jun", &mut data.last_outside_with_bot_at, 0..=i32::MAX);
+            bool_column!(cols[0], lang.t_mdrgp_last_slept_with_bot_title(), lang.t_mdrgp_last_slept_with_bot_desc(), &mut data.last_slept_with_bot);
+            stat_column!(cols[1], lang.t_mdrgp_last_woke_up_at_title(), lang.t_mdrgp_last_woke_up_at_desc(), &mut data.last_woke_up_at, 0..=i32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_last_fucked_at_title(), lang.t_mdrgp_last_fucked_at_desc(), &mut data.last_fucked_at, 0..=i32::MAX);
         });
 
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            stat_column!(cols[0], "Last headpat", "The last time (In game minutes) when you headpatted Jun", &mut data.last_headpatted_at, 0..=i32::MAX);
-            stat_column!(cols[1], "Last hunger notification", "The last time (In game minutes) when you got an hunger notification", &mut data.last_hunger_info_at, 0..=i32::MAX);
-            stat_column!(cols[2], "Last mental notification", "The last time (In game minutes) when you got a mental health notification", &mut data.last_mental_health_info_at, 0..=i32::MAX);
+            stat_column!(cols[0], lang.t_mdrgp_last_interact_at_title(), lang.t_mdrgp_last_interact_at_desc(), &mut data.last_interact_at, 0..=i32::MAX);
+            stat_column!(cols[1], lang.t_mdrgp_last_equipment_at_title(), lang.t_mdrgp_last_equipment_at_desc(), &mut data.last_equipment_at, 0..=i32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_last_outside_with_bot_at_title(), lang.t_mdrgp_last_outside_with_bot_at_desc(), &mut data.last_outside_with_bot_at, 0..=i32::MAX);
         });
 
         #[rustfmt::skip]
         ui.columns(3, |cols| {
-            stat_column!(cols[0], "Last talk", "The last time (In game minutes) you talked with Jun", &mut data.last_talked_at, 0..=i32::MAX);
-            stat_column!(cols[2], "Last bot talk", "The last time (In game minutes) when Jun started a talk with you", &mut data.last_bot_started_talk_at, 0..=i32::MAX);
+            stat_column!(cols[0], lang.t_mdrgp_last_headpatted_at_title(), lang.t_mdrgp_last_headpatted_at_desc(), &mut data.last_headpatted_at, 0..=i32::MAX);
+            stat_column!(cols[1], lang.t_mdrgp_last_hunger_info_at_title(), lang.t_mdrgp_last_hunger_info_at_desc(), &mut data.last_hunger_info_at, 0..=i32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_last_mental_health_info_at_title(), lang.t_mdrgp_last_mental_health_info_at_desc(), &mut data.last_mental_health_info_at, 0..=i32::MAX);
+        });
+
+        #[rustfmt::skip]
+        ui.columns(3, |cols| {
+            stat_column!(cols[0], lang.t_mdrgp_last_talked_at_title(), lang.t_mdrgp_last_talked_at_desc(), &mut data.last_talked_at, 0..=i32::MAX);
+            stat_column!(cols[2], lang.t_mdrgp_last_bot_started_talk_at_title(), lang.t_mdrgp_last_bot_started_talk_at_desc(), &mut data.last_bot_started_talk_at, 0..=i32::MAX);
         });
     }
 }
